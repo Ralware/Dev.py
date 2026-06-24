837b320e00fd19e7ed3af8ccd97f137f5bc4fd85
Ral <ral.vulns@gmail.com>
2026-06-24 18:11:43 +0530

    Some Functions And Round()

--- Functions,Variables.py
+++ Functions,Variables.py
@@ -1,12 +1,29 @@
-print("Hello, World !")
+# Global Variables
 
-Number = int(input("Enter A Number : "))
+Number = int(input("Enter A \"Number\" : "))
+Array = [];
+
+#Defined Functions 
+
+def InputName():
+    Name = input("Enter Your Name : ").strip().title()
+    return f"Hello, {Name}"
 
 def FactorialCalculator():
     Factorial = 1;
     for No in range(1,Number+1):
         Factorial*=No
-    return Factorial
-    
+        Array.append(No)
+    return Factorial    
 
-print(f"Factorial OF {Number} Is : {FactorialCalculator()}")
+def FindSum():
+    Num1 = float(input("Enter The First Number : "))
+    Num2 = float(input("Enter The Second Number : "))
+    return f"The Sum Of {Num1:.2f} & {Num2:.2f} Is : {round(Num1+Num2):,}"
+
+# Final Output Prints
+
+print(f"Factorial Of {Number} Is : {FactorialCalculator()}")
+print("Factorial Numbers : ",Array)
+print(InputName())
+print(FindSum())
