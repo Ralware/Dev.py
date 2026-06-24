a1cd02afba579b223a8116b699661a9edd667c83
Ral <ral.vulns@gmail.com>
2026-06-24 19:07:54 +0530

    Conditionals And Next Loops

---
+++ Conditionals.py
@@ -0,0 +1,30 @@
+GuessNumber = 67
+
+while True:
+    Guess = int(input("What's The Number : "))
+    if (Guess==GuessNumber):
+        print("You Got It!")
+        break
+    else:
+        print("Try Again!")
+
+def IsEven(Num):
+    return True if Num % 2 == 0 else False
+
+def InputNum():
+    Num = int(input("Enter A Number : "))
+    if (IsEven()):
+        return ("Even")
+    else:
+        return ("Odd")
+
+def SearchNames():
+    Name = input("What's Your Name : ")
+
+    match Name:
+        case "Harry":
+            return ("Toronto")
+        case "Ron":
+            return ("New York")
+        case _:
+            return ("Unknown")
