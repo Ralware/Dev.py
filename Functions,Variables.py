af836760e4d108ca6e1f5f87166f0d621aa75c00
Ral <ral.vulns@gmail.com>
2026-06-24 15:51:29 +0530

    Small Revision

--- Functions,Variables.py
+++ Functions,Variables.py
@@ -1,1 +1,12 @@
 print("Hello, World !")
+
+Number = int(input("Enter A Number : "))
+
+def FactorialCalculator():
+    Factorial = 1;
+    for No in range(1,Number+1):
+        Factorial*=No
+    return Factorial
+    
+
+print(f"Factorial OF {Number} Is : {FactorialCalculator()}")
