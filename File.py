c3265ec003efaab8c410479510453f35f5d6bf12
Ralware <ral.vulns@gmail.com>
2026-06-28 09:49:23 +0530

    Better File Read And Write Functions

--- File.py
+++ File.py
@@ -1,4 +1,18 @@
-file = open("Data.txt","a")
-for Num in range(0,1001):
-    file.write(f"Current Iteration : {Num}\n")
-file.close()
+# File Inputs 
+def InputData():
+    Count = int(input("How Many Names ? :"))
+    with open("Data.txt","a") as file:
+        for Index in range(1,Count+1):
+            Name = input(f"Name ({Index}): ")
+            file.write(Name+"\n") # Without \n , it'll be appended like Name1Name2Name3
+
+def ReadData():
+    NamesArr = []
+    with open("Data.txt","r") as file:
+        for line in file:
+            NamesArr.append(line.rstrip())
+    for Name in sorted(NamesArr):
+        print(Name)
+
+
+
